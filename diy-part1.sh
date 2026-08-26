1969ffc4bcbb7bb04b1cca7ec15eaca2f41ca7ec
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-26 23:40:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,3 +2,8 @@
 
 # 添加第三方软件源（fanchmwrt 多包合集，必须用 feeds 方式添加）
 echo "src-git fanchm https://github.com/fanchmwrt/fanchmwrt-packages.git" >> feeds.conf.default
+
+# 2. 添加 iStore 官方源和依赖源
+echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+echo "src-git nas https://github.com/linkease/nas-packages.git;master" >> feeds.conf.default
+echo "src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main" >> feeds.conf.default
