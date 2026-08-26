c85cb3e74c669836fadfca76ea8b22e35172338f
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-26 23:21:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# 添加第三方软件源，比如 fwx 插件源
-# 请把下面的地址替换为你使用的 fwx 插件真实的 Git 仓库地址！
+# 添加第三方软件源（请将下方地址替换为 fwx 插件的真实 Git 仓库地址）
 git clone https://github.com/你的fwx仓库地址.git package/fwx
-
-# （如果需要添加其他特殊软件源，也可以在这里加上 git clone ...）
