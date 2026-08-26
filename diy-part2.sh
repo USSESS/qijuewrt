a85b530c1cf7ca8f9acd0fc59cdb895016d470c9
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-26 23:31:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# 1. 将系统内所有的 ImmortalWrt 字样替换为 KiJueWrt
+# 1. 替换系统所有 ImmortalWrt 字样为 KiJueWrt
 sed -i 's/ImmortalWrt/KiJueWrt/g' package/base-files/files/etc/openwrt_release
 sed -i 's/ImmortalWrt/KiJueWrt/g' package/base-files/files/etc/banner
 grep -r "ImmortalWrt" feeds/luci -l 2>/dev/null | xargs sed -i 's/ImmortalWrt/KiJueWrt/g' 2>/dev/null
 
-# 2. 修改默认 IP (取消注释生效，默认改为 192.168.50.5)
-#sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 2. 修改默认 IP 为 10.10.10.1 (取消此行注释即可生效)
+# sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
-# 3. 修改默认主题 (可选)
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 4. 修改 Hostname (取消注释生效)
+# 3. 修改 Hostname 为 KiJueWrt (取消此行注释即可生效)
 # sed -i 's/OpenWrt/KiJueWrt/g' package/base-files/files/bin/config_generate
 
-# 5. 设置 x86-64 架构
+# 4. 设置 x86-64 架构
 echo 'CONFIG_TARGET_x86_64=y' >> .config
 echo 'CONFIG_TARGET_x86_64_Generic=y' >> .config
 
-# 6. 启用负载均衡插件 mwan3
+# 5. 启用负载均衡插件 mwan3
 echo 'CONFIG_PACKAGE_luci-app-mwan3=y' >> .config
 echo 'CONFIG_PACKAGE_mwan3=y' >> .config
 
-# 7. 启用 fwx 相关插件
+# 6. 启用 fwx 相关插件 (包含截图新增的 app-center 和 network)
 echo 'CONFIG_PACKAGE_luci-app-fwx-dashboard=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-fwx-dashboard-setting=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-fwx-feature=y' >> .config
@@ -33,3 +30,5 @@ echo 'CONFIG_PACKAGE_luci-app-fwx-record-whitelist=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-fwx-resources=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-fwx-system=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-fwx-user-record=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-fwx-app-center=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-fwx-network=y' >> .config
