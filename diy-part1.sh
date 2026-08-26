dfa8799236eca07a7e43b7ebf9388f0dea123635
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-27 01:32:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,8 +8,5 @@ echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.defa
 echo "src-git nas https://github.com/linkease/nas-packages.git;master" >> feeds.conf.default
 echo "src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main" >> feeds.conf.default
 
-# 克隆 luci-theme-edge 主题源码（需要使用 master 分支）
-git clone -b master https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
-
-# 下载背景图并放到 edge 主题的默认背景目录（目录可能需随主题版本微调，通常是 htdocs/luci-static/edge/background/）
-curl -L -o package/luci-theme-edge/htdocs/luci-static/edge/background/background.jpg "你的图片链接"
+# 克隆你自己分叉的 luci-theme-edge 主题（图片已包含在这个仓库里）
+git clone -b master https://github.com/USSESS/luci-theme-edge.git package/luci-theme-edge
