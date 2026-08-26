c482d83ba43b7abd12f72d754ae673a88c8a8442
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-26 23:19:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加第三方软件源，比如 fwx 插件源
+# 请把下面的地址替换为你使用的 fwx 插件真实的 Git 仓库地址！
+git clone https://github.com/你的fwx仓库地址.git package/fwx
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# （如果需要添加其他特殊软件源，也可以在这里加上 git clone ...）
