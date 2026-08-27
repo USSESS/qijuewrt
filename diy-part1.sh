530da450e6774d8a22de2d8e98ed8327a456594c
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-27 22:28:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-
 # iStore软件中心源
 echo "src-git istore https://github.com/linkease/istore.git;main" >> feeds.conf.default
-# 拉取你的定制edge主题，【重要：分支改为master！！】
+# 拉取定制edge主题 master分支
 echo "src-git edge_theme https://github.com/USSESS/luci-theme-edge.git;master" >> feeds.conf.default
+
+# 更新feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 echo "diy‑part1 完成"
