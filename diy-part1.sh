d2a26f3144002134433263fcc5625a46268c276b
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-27 23:01:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
+
 # iStore软件中心源
 echo "src-git istore https://github.com/linkease/istore.git;main" >> feeds.conf.default
-# 拉取定制edge主题 master分支
-echo "src-git edge_theme https://github.com/USSESS/luci-theme-edge.git;master" >> feeds.conf.default
+
+# 【已全部删除远程拉取edge主题代码，完全使用本仓库本地修改好的主题，不会被网上源码覆盖】
+# 把仓库内package下整套edge主题复制到编译环境
+cp -r $GITHUB_WORKSPACE/package/luci-theme-edge-master package/luci-theme-edge
 
 # 更新feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-echo "diy‑part1 完成"
+echo "diy‑part1.sh执行完毕：本地定制edge主题已导入"
