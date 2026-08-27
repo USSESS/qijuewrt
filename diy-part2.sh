1fcbfd8832b988913f88207697d4590e6334d3f6
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-28 01:23:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -e
+# ========== 拉取iStoreOS软件源，编译内置iStore商店 ==========
+git clone https://github.com/istoreos/istoreos-feed package/istoreos-feed
+echo "src-git istoreos https://github.com/istoreos/istoreos-feed.git" >> feeds.conf.default
+./scripts/feeds update istoreos
+./scripts/feeds install -a -p istoreos
+
+# 强制安装中文包、mwan3负载均衡进固件
+./scripts/feeds install luci-i18n-base-zh-cn
+./scripts/feeds install luci-i18n-argon-config-zh-cn
+./scripts/feeds install mwan3-nft
+
 # ==========系统全局名称 KiJueWrt ==========
 # 修改openwrt_release 系统信息（源码层）
 sed -i 's|ImmortalWrt|KiJueWrt|g' package/base-files/files/etc/openwrt_release
@@ -15,7 +26,7 @@ sed -i 's/set system.@system\[-1\].hostname=.*/set system.@system[0].hostname='\
 
 # ==========新建自定义uci-default脚本，开机自动执行==========
 cat > package/base-files/files/etc/uci-defaults/99-kijuewrt <<"UCIEOF"
-uci set luci.main.lang='zh_cn'
+uci set luci.main.lang='zh-cn'
 uci set system.@system[0].timezone='CST-8'
 uci set system.@system[0].zonename='Asia/Shanghai'
 uci set luci.main.mediaurlbase='/luci-static/edge'
