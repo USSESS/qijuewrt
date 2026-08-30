6afa82e87042801c0d0a5b2780e5c2ff345ceeae
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-30 23:28:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,15 +4,6 @@ set -e
 echo "src-git netwizard https://github.com/sirpdboy/luci-app-netwizard.git;main" >> feeds.conf.default
 echo "src-git istore https://github.com/linkease/istore.git;main" >> feeds.conf.default
 
-# 复制edge主题
-if [ -d "$GITHUB_WORKSPACE/luci-theme-edge-master" ];then
-    cp -r "$GITHUB_WORKSPACE/luci-theme-edge-master" package/luci-theme-edge
-    echo "OK:Edge主题复制完成"
-else
-    echo "ERROR:缺失edge主题文件夹"
-    exit 1
-fi
-
 # KiJueWrt主题
 mv luci-theme-kijue package/
 echo "OK:KiJueWrt主题移动完成"
