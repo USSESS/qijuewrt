7747d5bd441f00b094389c0d6fed6bab2e0ca97e
xiaoming <133641824+USSESS@users.noreply.github.com>
2026-08-30 23:21:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 set -e
-
-# 添加feeds源：netwizard一键向导
+# 添加feeds源
 echo "src-git netwizard https://github.com/sirpdboy/luci-app-netwizard.git;main" >> feeds.conf.default
-# 添加iStore feeds源，不再git clone移动文件，直接feeds拉取，彻底规避mv路径错误
 echo "src-git istore https://github.com/linkease/istore.git;main" >> feeds.conf.default
 
-#复制你仓库本地edge主题
+# 复制edge主题
 if [ -d "$GITHUB_WORKSPACE/luci-theme-edge-master" ];then
-    cp -r "$GITHUB_WORKSPACE/luci-theme-edge-master" package/luci-theme-edge
+    cp -r "$GITHUB_WORKSPACE/luci-theme-edge-master" package/luci-theme-kijue
     echo "OK:Edge主题复制完成"
 else
-    echo "ERROR:缺失edge主题文件夹 luci-theme-edge-master"
+    echo "ERROR:缺失edge主题文件夹"
     exit 1
 fi
+
+# KiJueWrt主题（移到这里）
+mv luci-theme-kijue package/
+echo "OK:KiJueWrt主题移动完成"
 
 # 更新安装feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 加上这一行
-mv luci-theme-kijue package/
-
-echo "diy‑part1.sh执行完毕"
+echo "diy-part1.sh执行完毕"
